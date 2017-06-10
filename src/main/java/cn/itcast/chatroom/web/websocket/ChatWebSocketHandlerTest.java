796b10ea23414430158d844f52c7a481368c7fbd
huangyueran <huangyueran@163.com>
2017-06-10 12:34:24 +0800

    WebSocket 通信

---
+++ src/main/java/cn/itcast/chatroom/web/websocket/ChatWebSocketHandlerTest.java
@@ -0,0 +1,46 @@
+package cn.itcast.chatroom.web.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * 
+ * 说明：WebSocket处理器
+ */
+@Component("chatWebSocketHandler")
+public class ChatWebSocketHandlerTest implements WebSocketHandler {
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean supportsPartialMessages() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
